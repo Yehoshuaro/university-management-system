223a7c97ec8f8199029976464929bff11323b97f
b_baccala <bayzulla96@gmail.com>
2024-11-03 00:03:58 +0500

    proper structure

--- src/main/java/com/example/coursemanagementsystem/Course/CourseService.java
+++
@@ -1,8 +0,0 @@
-package com.example.coursemanagementsystem.Course;
-
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CourseService {
-}
--- src/main/java/com/example/coursemanagementsystem/Student/StudentController.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursemanagementsystem.Student;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "student/")
-public class StudentController {
-}

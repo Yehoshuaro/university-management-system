846cff7d073cf25a47139f7b66cceecab25b7f3f
b_baccala <bayzulla96@gmail.com>
2024-11-04 02:02:41 +0500

    baizulla bakhtiyar: factory pattern is implemented

--- src/main/java/com/example/coursemanagementsystem/services/factory/GroupFactory.java
+++ src/main/java/com/example/coursemanagementsystem/services/factory/GroupFactory.java
@@ -1,4 +1,25 @@
 package com.example.coursemanagementsystem.services.factory;
 
+import com.example.coursemanagementsystem.dto.GroupDTO;
+import com.example.coursemanagementsystem.entities.Group;
+
 public class GroupFactory {
+
+    public static Group createGroup(GroupDTO groupDTO) {
+        return Group.builder()
+                .id(groupDTO.getId())
+                .name(groupDTO.getName())
+                .description(groupDTO.getDescription())
+                .year(groupDTO.getYear())
+                .build();
+    }
+
+    public static GroupDTO createGroupDTO(Group group) {
+        return GroupDTO.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .description(group.getDescription())
+                .year(group.getYear())
+                .build();
+    }
 }

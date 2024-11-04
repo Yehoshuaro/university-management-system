7e3451556fc0c6dfef5e45467e4efe01dfa715d1
Askat Narinbetov <231308@astanait.edu.kz>
2024-11-04 17:20:35 +0500

    added comments

--- src/main/java/com/example/coursemanagementsystem/dto/UserDTO.java
+++ src/main/java/com/example/coursemanagementsystem/dto/UserDTO.java
@@ -1,7 +1,11 @@
 package com.example.coursemanagementsystem.dto;
 
 import lombok.*;
-
+/**
+ * Data Transfer Object (DTO) for representing course information.
+ * This class is used to transfer course data between different layers of the application,
+ * such as between the controller and the service layer.
+ */
 @Getter
 @Setter
 @NoArgsConstructor

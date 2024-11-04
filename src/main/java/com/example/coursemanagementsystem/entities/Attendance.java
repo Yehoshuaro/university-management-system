30711e663227ff9f5788fd1a68094b541817b6d9
Askat Narinbetov <231308@astanait.edu.kz>
2024-11-04 10:19:49 +0500

    Added attendance as an entity.

---
+++ src/main/java/com/example/coursemanagementsystem/entities/Attendance.java
@@ -0,0 +1,29 @@
+package com.example.coursemanagementsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "attendances")
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+    private LocalDate date;
+    private boolean attended;
+}

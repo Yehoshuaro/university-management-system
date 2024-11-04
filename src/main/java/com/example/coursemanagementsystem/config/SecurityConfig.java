d46713a77c900221d448a422de1589e380b06079
Ернар <230709@astanait.edu.kz>
2024-11-04 16:53:09 +0500

    Fixed 403 error

--- src/main/java/com/example/coursemanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/example/coursemanagementsystem/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().anonymous()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)

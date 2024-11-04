24af84007af3f5e81392991492ecfef1465af945
Askat Narinbetov <231308@astanait.edu.kz>
2024-11-04 12:08:49 +0500

    pushed

--- src/main/java/com/example/coursemanagementsystem/services/serviceInterface/AttendanceServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursemanagementsystem.services.serviceInterface;
-
-import com.example.coursemanagementsystem.entities.Attendance;
-
-public interface AttendanceServiceInterface {
-    Attendance markAttendance(Integer student_id, Long course_id, boolean attended);
-}
